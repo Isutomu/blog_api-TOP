97464ab64e73541576aea72b0eba77242e1dd855
Isutomu <isutomu@gmail.com>
2025-01-11 16:07:26 -0300

    :sparkles: feat: create error page for unknown route

--- frontend-visitor/src/utils/routes.jsx
+++ frontend-visitor/src/utils/routes.jsx
@@ -1,10 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainPage from "../pages/MainPage/MainPage";
+import MainPage from "../pages/Main/Main";
+import PageNotFound from "../pages/PageNotFound/PageNotFound";
 
 const routes = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
+    errorElement: <PageNotFound />,
   },
 ]);
 
