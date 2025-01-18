5f321474523e70f54b191788e88696a43fdfd5e7
Isutomu <isutomu@gmail.com>
2025-01-18 12:32:30 -0300

    :bug: fix: Refer 'tag.name' property instead of 'tag' object

--- frontend-visitor/src/components/TagsAssembler/TagsAssembler.jsx
+++ frontend-visitor/src/components/TagsAssembler/TagsAssembler.jsx
@@ -6,8 +6,8 @@ function TagsAssembler({ tags }) {
   return (
     <div className={styles.tagsAssembler}>
       {tags.map((tag, index) => (
-        <Link className={styles.tag} key={index} to={`/tags/${tag}`}>
-          {tag}
+        <Link className={styles.tag} key={index} to={`/tags/${tag.name}`}>
+          {tag.name}
         </Link>
       ))}
     </div>
