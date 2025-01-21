b48930f2d29ec9a552fe58ddf8679dd9f83cd8b2
Isutomu <isutomu@gmail.com>
2025-01-21 12:21:55 -0300

    :sparkles: feat: Add logIn
    The code is pretty disorganized at the moment, specialize the
    "protected" routes situation at the routes.
    This will be refactored after the basic functionalities of the
    "Creator" frontend are implemented.

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -1,12 +1,32 @@
 const { Router } = require("express");
 const controllersGetPosts = require("../controllers/get/posts");
 const controllersPostPosts = require("../controllers/post/posts");
+const controllersPostLogIn = require("../controllers/post/logIn");
+const passport = require("passport");
 
 const routes = Router();
 
+/**
+ * ------------------- UNPROTECTED ROUTES -------------------
+ */
 routes.get("/posts", controllersGetPosts.allPosts);
 routes.get("/posts/:postId", controllersGetPosts.singlePost);
 
 routes.post("/posts", controllersPostPosts.singlePost);
+routes.post("/login", controllersPostLogIn.logIn);
+
+// dev only
+const createUser = require("../buildingHelper/addUser");
+routes.post("/signUp", createUser.addUser);
+
+/**
+ * ------------------- PROTECTED ROUTES -------------------
+ */
+
+// Passport authentication
+require("../config/passport");
+routes.use(passport.authenticate("jwt", { session: false }));
+
+routes.get("/postsProtected", controllersGetPosts.allPosts);
 
 module.exports = routes;
